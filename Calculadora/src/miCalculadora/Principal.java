0b2f45f0964e5f1a71ee0b2b16ed043942e548e0
Roberto Ochoa <ioser_jebus@hotmail.com>
2014-11-24 11:47:05 -0700

    Inicializando el proyecto

---
+++ Calculadora/src/miCalculadora/Principal.java
@@ -0,0 +1,11 @@
+package miCalculadora;
+
+public class Principal {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Programa de Calculadora");
+		System.out.println("=======================");
+	}
+
+}
