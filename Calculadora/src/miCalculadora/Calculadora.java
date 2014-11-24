ae42494330d9757034075201debddac5a9a45044
Roberto Ochoa <ioser_jebus@hotmail.com>
2014-11-24 11:56:07 -0700

    Agregando la suma a la calculadora

---
+++ Calculadora/src/miCalculadora/Calculadora.java
@@ -0,0 +1,8 @@
+package miCalculadora;
+
+public class Calculadora {
+	public double suma(double x,double y)
+	{
+		return x+y;
+	}
+}
--- Calculadora/src/miCalculadora/Principal.java
+++ Calculadora/src/miCalculadora/Principal.java
@@ -3,9 +3,12 @@ package miCalculadora;
 public class Principal {
 	
 	public static void main(String[] args){
+		Calculadora c=new Calculadora();
 		
 		System.out.println("Programa de Calculadora");
 		System.out.println("=======================");
+		
+		System.out.println("La suma de 2+3="+c.suma(2,3));
 	}
 
 }
