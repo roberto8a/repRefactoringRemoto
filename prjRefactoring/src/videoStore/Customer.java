e6864b77be14dc8244fec665ad68ad7e90ea7e1c
Roberto Ochoa <ioser_jebus@hotmail.com>
2014-12-03 11:53:16 -0700

    Refactorizando Renta

--- prjRefactoring/src/videoStore/Customer.java
+++ prjRefactoring/src/videoStore/Customer.java
@@ -32,37 +32,19 @@ public class Customer {
 		while (rentals.hasMoreElements())
 		{
 			double thisAmount	= 0;
-			Rental each			= (Rental) rentals.nextElement();
+			Rental aRental			= (Rental) rentals.nextElement();
 			
-			//determine amounts for each line
-			switch (each.getMovie().getPriceCode())
-			{
-				case Movie.REGULAR: 
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-						thisAmount += (each.getDaysRented()-2) * 1.5;
-					break;
-				
-				case Movie.NEW_RELEASE:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-					
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented()>3)
-						thisAmount += (each.getDaysRented()-3) * 1.5;
-					break;
-			}
+			thisAmount = amountFor(thisAmount, aRental);
 			
 			//add frequent renter points
 			frequentRenterPoints++;
 			
 			//add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode()==Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			if ((aRental.getMovie().getPriceCode()==Movie.NEW_RELEASE) && aRental.getDaysRented() > 1)
 				frequentRenterPoints++;
 			
 			//show figures for this rental
-			result+= "\t" + each.getMovie().getTitle() + "\t"+String.valueOf(thisAmount) + "\n";
+			result+= "\t" + aRental.getMovie().getTitle() + "\t"+String.valueOf(thisAmount) + "\n";
 			
 			totalAmount +=thisAmount;
 		}
@@ -73,5 +55,12 @@ public class Customer {
 		
 		return result;
 		
+	}
+
+	/**
+	 * @deprecated Use {@link videoStore.Rental#amountFor(double)} instead
+	 */
+	private double amountFor(double thisAmount, Rental each) {
+		return each.amountFor(thisAmount);
 	}
 }
